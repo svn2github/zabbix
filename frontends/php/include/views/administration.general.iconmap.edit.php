svn2github/zabbix

commit 22ab87729421ac1a93afe33931ad0c037e6656c9

A.F....... [ZBX-5927] fixed selectMappings returning a hash of mappings instead of an array in iconmap.get A.F....... [ZBX-5927] fixed preservekeys affecting selectSelements and selectLinks in map.get A.F....... [ZBX-5927] removed unused selectLinks parameter from CMapElement::getSelements() A......... [ZBX-5927] code refactoring
git-svn-id: svn://svn.zabbix.com/trunk@31864 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

frontends/php/include/views/administration.general.iconmap.edit.php
@@ -43,7 +43,8 @@ $iconMapTable->addRow(array(SPACE, SPACE, _('Inventory field'), _('Expression'),
 
 order_result($this->data['iconmap']['mappings'], 'sortorder');
 $i = 1;
-foreach ($this->data['iconmap']['mappings'] as $iconmappingid => $mapping) {
+foreach ($this->data['iconmap']['mappings'] as $mapping) {
+	$iconmappingid = $mapping['iconmappingid'];
 	$numSpan = new CSpan($i++.':');
 	$numSpan->addClass('rowNum');
 
@@ -52,6 +53,8 @@ foreach ($this->data['iconmap']['mappings'] as $iconmappingid => $mapping) {
 
 	$expressionTextBox = new CTextBox('iconmap[mappings]['.$iconmappingid.'][expression]', $mapping['expression']);
 	$expressionTextBox->setAttribute('maxlength', 64);
+
+	$iconMappingIdVar = new CVar('iconmap[mappings]['.$iconmappingid.'][iconmappingid]', $mapping['iconmappingid']);
 
 	$iconsComboBox = new CComboBox('iconmap[mappings]['.$iconmappingid.'][iconid]', $mapping['iconid']);
 	$iconsComboBox->addClass('mappingIcon');
@@ -64,7 +67,7 @@ foreach ($this->data['iconmap']['mappings'] as $iconmappingid => $mapping) {
 		new CSpan(null, 'ui-icon ui-icon-arrowthick-2-n-s move'),
 		$numSpan,
 		$profileLinksComboBox,
-		$expressionTextBox,
+		array($expressionTextBox, $iconMappingIdVar),
 		$iconsComboBox,
 		$iconPreviewImage,
 		new CButton('remove', _('Remove'), '', 'link_menu removeMapping'),

frontends/php/adm.iconmapping.php
@@ -64,8 +64,7 @@ if (isset($_REQUEST['save'])) {
 		: array();
 
 	$i = 0;
-	foreach ($_REQUEST['iconmap']['mappings'] as $iconmappingid => &$mapping) {
-		$mapping['iconmappingid'] = $iconmappingid;
+	foreach ($_REQUEST['iconmap']['mappings'] as &$mapping) {
 		$mapping['sortorder'] = $i++;
 	}
 	unset($mapping);

frontends/php/api/classes/CIconMap.php
@@ -216,7 +216,7 @@ class CIconMap extends CZBXAPI {
 		if (!is_null($options['selectMappings']) && str_in_array($options['selectMappings'], $subselectsAllowedOutputs)) {
 			$res = DBselect('SELECT imp.* FROM icon_mapping imp WHERE '.DBcondition('imp.iconmapid', $iconMapids));
 			while ($mapping = DBfetch($res)) {
-				$result[$mapping['iconmapid']]['mappings'][$mapping['iconmappingid']] = $mapping;
+				$result[$mapping['iconmapid']]['mappings'][] = $mapping;
 			}
 		}
 
@@ -327,7 +327,8 @@ class CIconMap extends CZBXAPI {
 			'selectMappings' => API_OUTPUT_EXTEND
 		));
 
-		$mappingsCreate = $mappingsUpdate = $mappingidsDelete = array();
+		$oldIconMappings = array();
+		$newIconMappings = array();
 		foreach ($iconMaps as $iconMap) {
 			if (!isset($iconMapsUpd[$iconMap['iconmapid']])) {
 				self::exception(ZBX_API_ERROR_PARAMETERS, _s('Icon map with iconmapid "%s" does not exist.', $iconMap['iconmapid']));
@@ -349,25 +350,13 @@ class CIconMap extends CZBXAPI {
 
 			if (isset($iconMap['mappings'])) {
 				$mappingsDb = $iconMapsUpd[$iconMap['iconmapid']]['mappings'];
-
+				foreach ($mappingsDb as $mapping) {
+					$oldIconMappings[] = $mapping;
+				}
 				foreach ($iconMap['mappings'] as $mapping) {
 					$mapping['iconmapid'] = $iconMap['iconmapid'];
-
-					if (isset($mapping['iconmappingid']) && isset($mappingsDb[$mapping['iconmappingid']])) {
-						$iconmappingid = $mapping['iconmappingid'];
-						unset($mapping['iconmappingid']);
-						$mappingsUpdate[] = array(
-							'values' => $mapping,
-							'where' => array('iconmappingid' => $iconmappingid)
-						);
-						unset($mappingsDb[$iconmappingid]);
-					}
-					else {
-						$mappingsCreate[] = $mapping;
-					}
+					$newIconMappings[] = $mapping;
 				}
-
-				$mappingidsDelete = array_merge($mappingidsDelete, array_keys($mappingsDb));
 			}
 
 			$iconMapid = $iconMap['iconmapid'];
@@ -380,12 +369,8 @@ class CIconMap extends CZBXAPI {
 			}
 		}
 
-		DB::update('icon_map', $updates);
-		DB::insert('icon_mapping', $mappingsCreate);
-		DB::update('icon_mapping', $mappingsUpdate);
-		if (!empty($mappingidsDelete)) {
-			DB::delete('icon_mapping', array('iconmappingid' => $mappingidsDelete));
-		}
+		DB::save('icon_map', $iconMaps);
+		DB::replace('icon_mapping', $oldIconMappings, $newIconMappings);
 
 		return array('iconmapids' => $iconMapids);
 	}

frontends/php/api/classes/CMap.php
@@ -387,12 +387,7 @@ class CMap extends CMapElement {
 				if (!isset($result[$selement['sysmapid']]['selements'])) {
 					$result[$selement['sysmapid']]['selements'] = array();
 				}
-				if (!is_null($options['preservekeys'])) {
-					$result[$selement['sysmapid']]['selements'][$selement['selementid']] = $selement;
-				}
-				else {
-					$result[$selement['sysmapid']]['selements'][] = $selement;
-				}
+				$result[$selement['sysmapid']]['selements'][] = $selement;
 			}
 		}
 
@@ -436,13 +431,7 @@ class CMap extends CMapElement {
 				if (!isset($result[$link['sysmapid']]['links'])) {
 					$result[$link['sysmapid']]['links'] = array();
 				}
-
-				if (!is_null($options['preservekeys'])) {
-					$result[$link['sysmapid']]['links'][$link['linkid']] = $link;
-				}
-				else {
-					$result[$link['sysmapid']]['links'][] = $link;
-				}
+				$result[$link['sysmapid']]['links'][] = $link;
 			}
 		}
 

frontends/php/api/classes/CMapElement.php
@@ -76,7 +76,6 @@ abstract class CMapElement extends CZBXAPI {
 			// output
 			'output'					=> API_OUTPUT_REFER,
 			'selectUrls'				=> null,
-			'selectLinks'				=> null,
 			'countOutput'				=> null,
 			'preservekeys'				=> null,
 			'sortfield'					=> '',
@@ -204,10 +203,6 @@ abstract class CMapElement extends CZBXAPI {
 
 				if (!isset($result[$selement['selementid']])) $result[$selement['selementid']]= array();
 
-				if (!is_null($options['selectLinks']) && !isset($result[$selement['selementid']]['links'])) {
-					$result[$selement['selementid']]['links'] = array();
-				}
-
 				if (!is_null($options['selectUrls']) && !isset($result[$selement['selementid']]['urls'])) {
 					$result[$selement['selementid']]['urls'] = array();
 				}
@@ -240,43 +235,6 @@ abstract class CMapElement extends CZBXAPI {
 			$dbSelementUrls = DBselect($sql);
 			while ($selementUrl = DBfetch($dbSelementUrls)) {
 				$result[$selementUrl['selementid']]['urls'][] = $selementUrl;
-			}
-		}
-// Adding Links
-		if (!is_null($options['selectLinks']) && str_in_array($options['selectLinks'], $subselectsAllowedOutputs)) {
-			$linkids = array();
-			$mapLinks = array();
-
-			$sql = 'SELECT sl.* FROM sysmaps_links sl WHERE '.DBcondition('sl.sysmapid', $sysmapids);
-			$dbLinks = DBselect($sql);
-			while ($link = DBfetch($dbLinks)) {
-				$link['linktriggers'] = array();
-
-				$mapLinks[$link['linkid']] = $link;
-				$linkids[$link['linkid']] = $link['linkid'];
-			}
-
-			$sql = 'SELECT DISTINCT slt.* FROM sysmaps_link_triggers slt WHERE '.DBcondition('slt.linkid', $linkids);
-			$dbLinkTriggers = DBselect($sql);
-			while ($linkTrigger = DBfetch($dbLinkTriggers)) {
-				$mapLinks[$linkTrigger['linkid']]['linktriggers'][] = $linkTrigger;
-			}
-
-			foreach ($mapLinks as $link) {
-				if (!isset($result[$link['selementid1']]['links']))
-					$result[$link['selementid1']]['links'] = array();
-
-				if (!isset($result[$link['selementid2']]['links']))
-					$result[$link['selementid2']]['links'] = array();
-
-				if (!is_null($options['preservekeys'])) {
-					$result[$link['selementid1']]['links'][$link['linkid']] = $link;
-					$result[$link['selementid2']]['links'][$link['linkid']] = $link;
-				}
-				else{
-					$result[$link['selementid1']]['links'][] = $link;
-					$result[$link['selementid2']]['links'][] = $link;
-				}
 			}
 		}
 

frontends/php/map.php
@@ -144,6 +144,9 @@ if ($nocalculations) {
 	$allLinks = true;
 }
 else {
+	// we need selements to be a hash for further processing
+	$map['selements'] = zbx_toHash($map['selements'], 'selementid');
+
 	add_triggerExpressions($map['selements']);
 
 	$areas = populateFromMapAreas($map);

frontends/php/sysmap.php
@@ -254,6 +254,8 @@ while ($row = DBfetch($result)) {
 }
 order_result($iconList, 'name');
 
+// we need selements to be a hash for further processing
+$sysmap['selements'] = zbx_toHash($sysmap['selements'], 'selementid');
 zbx_add_post_js('ZABBIX.apps.map.run("sysmap_cnt", '.CJs::encodeJson(array(
 	'sysmap' => $sysmap,
 	'iconList' => $iconList,

commit 259dd40cc58905e318becc2f4168e81fb45a7860

..F....... [ZBX-6635] fixed undefined index errors when trying to save a value mapping with an empty expression
Before the fix:
- When you try to save a value mapping with an empty expression multiple undefined index error messages appear. Broken by r31864.

After the fix:
- The errors have been fixed.

git-svn-id: svn://svn.zabbix.com/trunk@36019 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

frontends/php/include/views/administration.general.iconmap.edit.php
@@ -42,21 +42,22 @@ if (isset($this->data['iconmapid'])) {
 $iconMapTable->addRow(array(SPACE, SPACE, _('Inventory field'), _('Expression'), _('Icon'), SPACE, SPACE));
 
 order_result($this->data['iconmap']['mappings'], 'sortorder');
-$i = 1;
+$i = 0;
 foreach ($this->data['iconmap']['mappings'] as $mapping) {
-	$iconmappingid = $mapping['iconmappingid'];
-	$numSpan = new CSpan($i++.':');
+	$numSpan = new CSpan(($i + 1).':');
 	$numSpan->addClass('rowNum');
 
-	$profileLinksComboBox = new CComboBox('iconmap[mappings]['.$iconmappingid.'][inventory_link]', $mapping['inventory_link']);
+	$profileLinksComboBox = new CComboBox('iconmap[mappings]['.$i.'][inventory_link]', $mapping['inventory_link']);
 	$profileLinksComboBox->addItems($this->data['inventoryList']);
 
-	$expressionTextBox = new CTextBox('iconmap[mappings]['.$iconmappingid.'][expression]', $mapping['expression']);
+	$expressionTextBox = new CTextBox('iconmap[mappings]['.$i.'][expression]', $mapping['expression']);
 	$expressionTextBox->setAttribute('maxlength', 64);
+	$expressionTextBox = array($expressionTextBox);
+	if (isset($mapping['iconmappingid'])) {
+		$expressionTextBox[] = new CVar('iconmap[mappings]['.$i.'][iconmappingid]', $mapping['iconmappingid']);
+	}
 
-	$iconMappingIdVar = new CVar('iconmap[mappings]['.$iconmappingid.'][iconmappingid]', $mapping['iconmappingid']);
-
-	$iconsComboBox = new CComboBox('iconmap[mappings]['.$iconmappingid.'][iconid]', $mapping['iconid']);
+	$iconsComboBox = new CComboBox('iconmap[mappings]['.$i.'][iconid]', $mapping['iconid']);
 	$iconsComboBox->addClass('mappingIcon');
 	$iconsComboBox->addItems($this->data['iconList']);
 
@@ -67,13 +68,15 @@ foreach ($this->data['iconmap']['mappings'] as $mapping) {
 		new CSpan(null, 'ui-icon ui-icon-arrowthick-2-n-s move'),
 		$numSpan,
 		$profileLinksComboBox,
-		array($expressionTextBox, $iconMappingIdVar),
+		$expressionTextBox,
 		$iconsComboBox,
 		$iconPreviewImage,
 		new CButton('remove', _('Remove'), '', 'link_menu removeMapping'),
 	), 'sortable');
-	$row->setAttribute('id', 'iconmapidRow_'.$iconmappingid);
+	$row->setAttribute('id', 'iconmapidRow_'.$i);
 	$iconMapTable->addRow($row);
+
+	$i++;
 }
 
 