svn2github/zabbix

commit 42051d9bc2dafd043b9dd9938674a0ff6c186b52

A.F....... [ZBX-7781] fixed no error being triggered when trying to pass the "auth" parameter to a method that does not require authentication
Before the fix:
- When calling a method that does not require authentication  (apiiinfo.version or user.login), the authentication token could be omitted. But it was given, it was validated, and could trigger an error if it's invalid.

After the fix:
- Passing the authentication token will trigger an error.

Additionally, the API request handling code has been rewritten so that it would be more structured and could be at least partially covered by tests. Some of the refactoring changes are:
- The CZBXAPI class has been renamed to CApiService;
- The CLocalApiClient class has been introduced to handle calls to CApiService objects;
- The CFrontendApiWrapper has been introduced to convert the output of the API to the format required by the frontend

git-svn-id: svn://svn.zabbix.com/trunk@43976 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

frontends/php/api/rpc/class.cjsonrpc.php
@@ -25,18 +25,25 @@ class CJSONrpc {
 
 	public $json;
 
-	private $_multicall;
+	/**
+	 * API client to use for making requests.
+	 *
+	 * @var CApiClient
+	 */
+	protected $apiClient;
+
 	private $_error;
 	private $_response;
 	private $_error_list;
 	private $_zbx2jsonErrors;
 	private $_jsonDecoded;
 
-	public function __construct($jsonData) {
+	public function __construct(CApiClient $apiClient, $jsonData) {
+		$this->apiClient = $apiClient;
+
 		$this->json = new CJSON();
 		$this->initErrors();
 
-		$this->_multicall = false;
 		$this->_error = false;
 		$this->_response = array();
 		$this->_jsonDecoded = $this->json->decode($jsonData, true);
@@ -45,30 +52,23 @@ class CJSONrpc {
 			$this->jsonError(null, '-32700', null, null, true);
 			return;
 		}
-
-		if (!isset($this->_jsonDecoded['jsonrpc'])) {
-			$this->_multicall = true;
-		}
-		else {
-			$this->_jsonDecoded = array($this->_jsonDecoded);
-		}
 	}
 
 	public function execute($encoded = true) {
-		foreach ($this->_jsonDecoded as $call) {
-			// notification
-			if (!isset($call['id'])) {
-				$call['id'] = null;
-			}
+		$call = $this->_jsonDecoded;
 
-			if (!$this->validate($call)) {
-				continue;
-			}
+		// notification
+		if (!isset($call['id'])) {
+			$call['id'] = null;
+		}
 
+		if ($this->validate($call)) {
 			$params = isset($call['params']) ? $call['params'] : null;
 			$auth = isset($call['auth']) ? $call['auth'] : null;
 
-			$result = czbxrpc::call($call['method'], $params, $auth);
+			list($api, $method) = explode('.', $call['method']);
+			$result = $this->apiClient->callMethod($api, $method, $params, $auth);
+
 			$this->processResult($call, $result);
 		}
 
@@ -104,8 +104,8 @@ class CJSONrpc {
 		return true;
 	}
 
-	public function processResult($call, $result) {
-		if (isset($result['result'])) {
+	public function processResult($call, CApiClientResponse $response) {
+		if ($response->data) {
 			// Notifications MUST NOT be answered
 			if ($call['id'] === null) {
 				return;
@@ -113,23 +113,16 @@ class CJSONrpc {
 
 			$formedResp = array(
 				'jsonrpc' => self::VERSION,
-				'result' => $result['result'],
+				'result' => $response->data,
 				'id' => $call['id']
 			);
 
-			if ($this->_multicall) {
-				$this->_response[] = $formedResp;
-			}
-			else {
-				$this->_response = $formedResp;
-			}
+			$this->_response = $formedResp;
 		}
 		else {
-			$result['data'] = isset($result['data']) ? $result['data'] : null;
-			$result['debug'] = isset($result['debug']) ? $result['debug'] : null;
-			$errno = $this->_zbx2jsonErrors[$result['error']];
+			$errno = $this->_zbx2jsonErrors[$response->errorCode];
 
-			$this->jsonError($call['id'], $errno, $result['data'], $result['debug']);
+			$this->jsonError($call['id'], $errno, $response->errorMessage, $response->debug);
 		}
 	}
 
@@ -166,12 +159,7 @@ class CJSONrpc {
 			'id' => $id
 		);
 
-		if ($this->_multicall) {
-			$this->_response[] = $formed_error;
-		}
-		else {
-			$this->_response = $formed_error;
-		}
+		$this->_response = $formed_error;
 	}
 
 	private function initErrors() {

frontends/php/include/classes/class.cwebuser.php
@@ -84,7 +84,7 @@ class CWebUser {
 	public static function checkAuthentication($sessionid) {
 		try {
 			if ($sessionid !== null) {
-				self::$data = API::User()->checkAuthentication($sessionid);
+				self::$data = API::User()->checkAuthentication(array($sessionid));
 			}
 
 			if ($sessionid === null || empty(self::$data)) {
@@ -135,6 +135,15 @@ class CWebUser {
 	 */
 	public static function getType() {
 		return self::$data['type'];
+	}
+
+	/**
+	 * Returns true if debug mode is enabled.
+	 *
+	 * @return bool
+	 */
+	public static function getDebugMode() {
+		return (self::$data['debug_mode']);
 	}
 
 	/**

commit 4025dadf466cdacb81f59f2a26531eae24e8d5de

A......... [ZBX-7781] fixed remote API calls returning an error if a successful API call returns an empty result ..F....... [ZBX-7781] fixed undefined index on the login screen when the guest user is disabled
Regressions, broken in r43976.

git-svn-id: svn://svn.zabbix.com/trunk@44557 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

frontends/php/api/rpc/class.cjsonrpc.php
@@ -105,7 +105,12 @@ class CJSONrpc {
 	}
 
 	public function processResult($call, CApiClientResponse $response) {
-		if ($response->data) {
+		if ($response->errorCode) {
+			$errno = $this->_zbx2jsonErrors[$response->errorCode];
+
+			$this->jsonError($call['id'], $errno, $response->errorMessage, $response->debug);
+		}
+		else {
 			// Notifications MUST NOT be answered
 			if ($call['id'] === null) {
 				return;
@@ -118,11 +123,6 @@ class CJSONrpc {
 			);
 
 			$this->_response = $formedResp;
-		}
-		else {
-			$errno = $this->_zbx2jsonErrors[$response->errorCode];
-
-			$this->jsonError($call['id'], $errno, $response->errorMessage, $response->debug);
 		}
 	}
 

frontends/php/include/classes/class.cwebuser.php
@@ -121,7 +121,8 @@ class CWebUser {
 			'alias' => ZBX_GUEST_USER,
 			'userid' => 0,
 			'lang' => 'en_gb',
-			'type' => '0'
+			'type' => '0',
+			'debug_mode' => false
 		);
 	}
 